18aaddd633ca589331c6a53540bf5d752ca70578
Andreas Möller <andrmoel@gmail.com>
2019-08-19 19:29:43 +0200

    - Some code comments

--- public/run.php
+++ public/run.php
@@ -2,11 +2,14 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Create average cloud maps for 1 day
 $dccm = new \App\Controller\DailyCloudCoverageMapController();
 $dccm->run();
 
+// Mean average cloud maps als take 5 days (-/+2 days) into account
 $mdcm = new \App\Controller\MeanDailyCloudMapsController();
 $mdcm->run();
 
+// Create colored cloud maps of the 5 days cloud maps
 $cccm = new \App\Controller\ColorCloudCoverageMapController();
 $cccm->run();
